2df2966ababe585ea088e869010a557055c1ecc3
Garulf <535299+Garulf@users.noreply.github.com>
2022-02-25 05:17:55 -0500

    Add score threshold and empty settings

--- plugin/main.py
+++ plugin/main.py
@@ -10,11 +10,13 @@ def match(query, game_name):
 class EpicGamesStoreLauncher(Flox):
 
     def query(self, query):
+        score_threshold = int(self.settings.get('score_threshold', 10))
         try:
             games = egs.get_games()
             for game in games:
                 score = match(query, game.display_name)
-                if (score > 10 or query == '') and not game.b_is_incomplete_install:
+                show_on_empty_query = self.settings.get('show_on_empty_query', True)
+                if (score > score_threshold or (show_on_empty_query and query == '')) and not game.b_is_incomplete_install:
                     self.add_item(
                         title=game.display_name, 
                         subtitle=str(game.full_exe_path()),
