b9ebd042be56702cee4f498196b6beeb0b1f1819
Garulf <535299+Garulf@users.noreply.github.com>
2022-12-22 00:03:14 -0500

    Use Flox's string matcher

--- plugin/main.py
+++ plugin/main.py
@@ -1,35 +1,36 @@
 import webbrowser
 from difflib import SequenceMatcher as sm
 from flox import Flox, ICON_WARNING
+from flox.string_matcher import string_matcher
 
 import egs
 
-def match(query, game_name):
-    return int(sm(lambda x: x==" " or x=="-", query.lower(), game_name.lower()).ratio() * 100)
 
 class EpicGamesStoreLauncher(Flox):
 
     def query(self, query):
-        score_threshold = int(self.settings.get('score_threshold', 10))
         try:
             games = egs.get_games()
             for game in games:
-                score = match(query, game.display_name)
-                show_on_empty_query = self.settings.get('show_on_empty_query', True)
-                if (score > score_threshold or (show_on_empty_query and query == '')) and not game.b_is_incomplete_install:
+                match_data = string_matcher(query, game.display_name)
+                score = match_data.score if match_data else 0
+                show_on_empty_query = self.settings.get(
+                    'show_on_empty_query', True)
+                if not game.b_is_incomplete_install:
                     self.add_item(
-                        title=game.display_name, 
+                        title=game.display_name,
                         subtitle=str(game.full_exe_path()),
                         icon=str(game.full_exe_path()),
                         method=self.launch,
-                        parameters=[game.catalog_namespace, game.catalog_item_id, game.app_name],
-                        score=score
-                        )
+                        parameters=[game.catalog_namespace,
+                                    game.catalog_item_id, game.app_name],
+                        score=int(score)
+                    )
         except (FileNotFoundError):
             self.add_item(
                 title='Unable to locate Epic Games Launcher',
                 icon=ICON_WARNING
-                )
+            )
 
     def context_menu(self, data):
         pass
@@ -37,6 +38,7 @@ class EpicGamesStoreLauncher(Flox):
     def launch(self, namespace, catalog_id, app_name):
         uri = f'com.epicgames.launcher://apps/{namespace}%3A{catalog_id}%3A{app_name}?action=launch&silent=true'
         webbrowser.open(uri)
+
 
 if __name__ == "__main__":
     EpicGamesStoreLauncher()
